9670b427ae7da30fda81d2f01c6737ee1dda6c43
BrandonWedu <Brandon.education@proton.me>
2024-10-16 15:18:43 -0400

    implemented Client

---
+++ RetailClassLibrary/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailClassLibrary
+{
+    //Contains Client data and extends user
+    public class Client : User
+    {
+        //Fields
+        private int clientID;
+
+        //Constructor
+        public Client(int clientID, string firstName, string lastName, Address address, string phoneNumber, string email) : base(firstName, lastName, address, phoneNumber, email)
+        {
+            this.clientID = clientID;
+        }
+
+        //Get Set
+        public int ClientID
+        {
+            get { return this.clientID; }
+            set { this.clientID = value; }
+        }
+
+    }
+}
