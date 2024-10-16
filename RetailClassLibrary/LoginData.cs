718c70f493ad5829edf091b8f52433d47cc01b7a
BrandonWedu <Brandon.education@proton.me>
2024-10-16 15:55:26 -0400

    created LoginData to hold login info

---
+++ RetailClassLibrary/LoginData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailClassLibrary
+{
+    //Holds User Login information
+    public class LoginData
+    {
+        //Fields
+        private string username;
+        private string password;
+
+        //Constructor
+        public LoginData(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+
+        //Get
+        public string Username
+        {
+            get { return this.username; }
+            set { this.username = value; }
+        }
+        public string Password
+        {
+            get { return this.password; }
+            set { this.password = value; }
+        }
+    }
+}
