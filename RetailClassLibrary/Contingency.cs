676b4fca700b49cd07b0107aab0fd61bd3c49bb4
BrandonWedu <Brandon.education@proton.me>
2024-10-18 12:39:39 -0400

    Implemented Contingency

---
+++ RetailClassLibrary/Contingency.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailClassLibrary
+{
+    internal class Contingency : ICloneable<Contingency>
+    {
+        //Fields
+        private int? contengencyID;
+        private string description;
+
+        //Constructor without id
+        public Contingency(string description) 
+        {
+            contengencyID = null;
+            this.description = description;
+        }
+        //Constructor with id
+        public Contingency(int? contengencyID,  string description)
+        {
+            this.contengencyID = contengencyID;
+            this.description += description;
+        }
+
+        //GetSet
+        public int? ContengencyID
+        {
+            get { return contengencyID; }
+            set { contengencyID = value; }
+        }
+        public string Description
+        {
+            get { return description; } 
+            set { description = value; }
+        }
+
+        //Implement Interface
+        public Contingency DeepCopy()
+        {
+            return new Contingency(contengencyID, description);
+        }
+    }
+}
