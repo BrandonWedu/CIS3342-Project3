8ba411bf4a1d5c5dd13948576a092813f79a6d3f
BrandonWedu <Brandon.education@proton.me>
2024-10-28 12:16:10 -0400

    Home search transfer to edit Functional

--- Project3/HomeEdit.aspx.cs
+++ Project3/HomeEdit.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RetailClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,12 @@ namespace Project3
 {
     public partial class HomeEdit : System.Web.UI.Page
     {
+        Agent agent;
+        Home home;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            agent = (Agent)Session["Agent"];
+            home = (Home)Session["Home"];
         }
     }
 }
